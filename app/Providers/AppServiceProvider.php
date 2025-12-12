d55e8018822df46cb85184049622426faa78abcf
Njorse <foundnyj@gmail.com>
2025-12-12 09:38:56 -0500

    Arreglo SSL Render y mejoras UI

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,12 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any applicatio  n services.
+     * Register any application services.
      */
     public function register(): void
     {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
