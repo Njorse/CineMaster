ab105fc7a9d811449e748745e3cff9ac5f3cd23c
Nielz Jorsen <99045046+Njorse@users.noreply.github.com>
2025-12-12 09:31:08 -0500

    Remove HTTPS URL forcing in boot method
    Removed URL forcing for production environment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,27 +1,47 @@
 <?php
 
+
+
 namespace App\Providers;
 
+
+
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
+
+
 
 class AppServiceProvider extends ServiceProvider
+
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
 
     /**
-     * Bootstrap any application services.
+
+     * Register any applicatio  n services.
+
      */
-    public function boot(): void
+
+    public function register(): void
+
     {
-        if($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+
+        //
+
     }
+
+
+
+    /**
+
+     * Bootstrap any application services.
+
+     */
+
+    public function boot(): void
+
+    {
+
+        //
+
+    }
+
 }
